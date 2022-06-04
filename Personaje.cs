938e50b0702b0562c97451908bce244e869f1299
Zeta279 <equi27@live.com.ar>
2022-06-04 00:50:16 -0300

    Mostrando las características y datos de los personajes

--- Personaje.cs
+++ Personaje.cs
@@ -7,5 +7,26 @@ namespace JuegoRPG{
             this.carac = carac;
             this.dat = dat;
         }
+
+        public override string ToString()
+        {
+            string cadena = new string("");
+
+            cadena += "Datos:\n";
+            cadena += $"Tipo: {this.dat.Tipo}\n";
+            cadena += $"Nombre: {this.dat.Nombre}\n";
+            cadena += $"Apodo: {this.dat.Apodo}\n";
+            cadena += $"Fecha de nacimiento: {this.dat.FechaNac.ToString("dd/MM/yyyy")}\n";
+            cadena += $"Edad: {this.dat.Edad}\n\n";
+
+            cadena += "Características:\n";
+            cadena += $"Velocidad: {this.carac.Velocidad}\n";
+            cadena += $"Destreza: {this.carac.Destreza}\n";
+            cadena += $"Fuerza: {this.carac.Fuerza}\n";
+            cadena += $"Nivel: {this.carac.Nivel}\n";
+            cadena += $"Armadura: {this.carac.Armadura}\n";
+
+            return cadena;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,9 +3,24 @@
         static void Main(string[] args){
             int cantidadJugadores;
             List<Personaje> listaPersonajes = new List<Personaje>();
+            int jugadoresRestantes, player1, player2;
+            Random rnd = new Random();
 
             Console.WriteLine("Ingrese la cantidad de jugadores: ");
             cantidadJugadores = int.Parse(Console.ReadLine());
+            jugadoresRestantes = cantidadJugadores;
+
+            for (int i = 0; i < cantidadJugadores; i++)
+            {
+                listaPersonajes.Add(new Personaje(new Caracteristicas(), new Datos()));
+            }
+
+            Console.WriteLine($"Mostrando los datos de {cantidadJugadores} personajes\n");
+
+            foreach(Personaje p in listaPersonajes)
+            {
+                Console.WriteLine(p);
+            }
         }
     }
 }
